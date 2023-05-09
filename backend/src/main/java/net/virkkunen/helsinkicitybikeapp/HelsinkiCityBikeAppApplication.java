065beceb2cc6dd87d69cd94d0f89828b0faa88af
Solja Virkkunen <soljatuulia@gmail.com>
2023-05-09 11:15:19 +0300

    Add RestTemplate

--- backend/src/main/java/net/virkkunen/helsinkicitybikeapp/HelsinkiCityBikeAppApplication.java
+++ backend/src/main/java/net/virkkunen/helsinkicitybikeapp/HelsinkiCityBikeAppApplication.java
@@ -2,6 +2,9 @@ package net.virkkunen.helsinkicitybikeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class HelsinkiCityBikeAppApplication {
@@ -9,5 +12,10 @@ public class HelsinkiCityBikeAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelsinkiCityBikeAppApplication.class, args);
 	}
+
+  @Bean
+  public RestTemplate restTemplate(RestTemplateBuilder builder) {
+      return builder.build();
+  }   
 
 }
