03cca2563bdf23b6dc1d7ca727af50952e4f2ca5
Solja Virkkunen <soljatuulia@gmail.com>
2023-05-16 10:44:01 +0300

    Refactor station pagination

--- backend/src/main/java/net/virkkunen/backend/controllers/StationController.java
+++ backend/src/main/java/net/virkkunen/backend/controllers/StationController.java
@@ -2,12 +2,14 @@ package net.virkkunen.backend.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -16,7 +18,7 @@ import net.virkkunen.backend.entities.StationDetails;
 import net.virkkunen.backend.repositories.JourneyRepository;
 import net.virkkunen.backend.repositories.StationRepository;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:3000", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api")
 public class StationController {
@@ -27,12 +29,14 @@ public class StationController {
   @Autowired
   JourneyRepository journeyRepo;
 
-  @GetMapping("/stations")
-  public Page<Station> listStations(Pageable pageable) {
+  @GetMapping(value = "/stations", produces = "application/json")
+  public Page<Station> listStations(@RequestParam(value = "page", defaultValue = "0") int pageNumber,
+    @RequestParam(value = "size", defaultValue = "10") int pageSize ) {
+    Pageable pageable = PageRequest.of(pageNumber, pageSize);
     return stationRepo.findAll(pageable);
   }
 
-  @GetMapping("/stations/{id}")
+  @GetMapping(value = "/stations/{id}")
   public StationDetails getStationDetails(@PathVariable int id) {
     Station station = stationRepo.findById(id).orElse(null);
     
