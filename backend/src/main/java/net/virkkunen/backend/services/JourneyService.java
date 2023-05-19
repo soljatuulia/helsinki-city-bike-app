a80884a348152aeb44f44710b51580c09ec6f1b5
Solja Virkkunen <soljatuulia@gmail.com>
2023-05-19 11:42:01 +0300

    Improve data validation and error handling in JourneyService

--- backend/src/main/java/net/virkkunen/backend/services/JourneyService.java
+++ backend/src/main/java/net/virkkunen/backend/services/JourneyService.java
@@ -10,7 +10,6 @@ import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,60 +20,73 @@ import net.virkkunen.backend.repositories.JourneyRepository;
 @Service
 public class JourneyService {
 
-    @Autowired
     private JourneyRepository journeyRepository;
+
+    public JourneyService(JourneyRepository journeyRepository) {
+        this.journeyRepository = journeyRepository;
+    }
 
     @PostConstruct
     public void init() throws IOException {
-      try {
-        System.out.println("We are in JourneyService init()");
-        saveJourneysFromCsv("C://helsinki-city-bike-app//helsinki-city-bike-app//backend//src//main//resources//journeyslist.csv");
-      } catch (IOException ex) {
-        ex.printStackTrace();
-      }
+        String csvFilePath = "C://helsinki-city-bike-app//helsinki-city-bike-app//backend//src//main//resources//journeylist.csv";
+        try {
+            System.out.println("We are in JourneyService init()");
+            saveJourneysFromCsv(csvFilePath);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 
     @Transactional
     public void saveJourneysFromCsv(String csvFilePath) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(csvFilePath), "UTF-8"));
-        String line;
         List<Journey> journeys = new ArrayList<>();
-        
-        boolean firstLine = true; 
-        while ((line = br.readLine()) != null) {
-            if (firstLine) {
-                firstLine = false;
-                continue;
-            }
 
-            String[] fields = line.split(",");
-            try {
-              LocalDateTime departureTime = LocalDateTime.parse(fields[0], DateTimeFormatter.ISO_DATE_TIME);
-              LocalDateTime returnTime = LocalDateTime.parse(fields[1], DateTimeFormatter.ISO_DATE_TIME);
-              Integer departureStationId = Integer.parseInt(fields[2]);
-              String departureStationName = fields[3];
-              Integer returnStationId = Integer.parseInt(fields[4]);
-              String returnStationName = fields[5];
-              Integer distance = Integer.parseInt(fields[6]);
-              Integer duration = Integer.parseInt(fields[7]);
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(csvFilePath), "UTF-8"))) {
+            String line;
+            boolean firstLine = true;
+            while ((line = br.readLine()) != null) {
+                if (firstLine) {
+                    firstLine = false;
+                    continue;
+                }
 
-              if (departureTime.isBefore(returnTime) & departureStationId > 0 & distance >= 10 & duration >= 10) {            
-                Journey journey = new Journey(departureTime, returnTime, departureStationId,
-                departureStationName, returnStationId, returnStationName, distance,
-                duration);
-
-                journeys.add(journey);
-              }
-
-            } catch (NumberFormatException numEx) {
-              numEx.printStackTrace();
-            } catch (DateTimeParseException dateEx) {
-              dateEx.printStackTrace();
+                String[] fields = line.split(",");
+                try {
+                    parseAndAddValidJourney(fields, journeys);
+                } catch (NumberFormatException | DateTimeParseException ex) {
+                    ex.printStackTrace();
+                    throw ex;
+                }
             }
         }
 
         journeyRepository.saveAll(journeys);
-        br.close();
+    }
 
+    private void parseAndAddValidJourney(String[] fields, List<Journey> journeys) {
+        LocalDateTime departureTime = LocalDateTime.parse(fields[0], DateTimeFormatter.ISO_DATE_TIME);
+        LocalDateTime returnTime = LocalDateTime.parse(fields[1], DateTimeFormatter.ISO_DATE_TIME);
+        Integer departureStationId = Integer.parseInt(fields[2]);
+        String departureStationName = fields[3].replace("\"", "").trim();
+        Integer returnStationId = Integer.parseInt(fields[4]);
+        String returnStationName = fields[5].replace("\"", "").trim();
+        Integer distance = Integer.parseInt(fields[6]);
+        Integer duration = Integer.parseInt(fields[7]);
+
+        if (returnTime.isBefore(departureTime)) {
+            throw new IllegalArgumentException("Departure time must be earlier than return time");
+        } else if (departureStationId < 0 || returnStationId < 0) {
+            throw new IllegalArgumentException("Station id can not be negative");
+        } else if (distance < 10) {
+            throw new IllegalArgumentException("Distance must be over 10 meters");
+        } else if (duration < 10) {
+            throw new IllegalArgumentException("Duration must be over 10 seconds");
+        }
+
+        Journey journey = new Journey(departureTime, returnTime, departureStationId,
+                departureStationName, returnStationId, returnStationName, distance,
+                duration);
+
+        journeys.add(journey);
     }
 }
