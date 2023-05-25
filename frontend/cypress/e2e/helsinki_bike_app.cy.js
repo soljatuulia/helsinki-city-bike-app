a7d4f5f82847d356d16a8d8c5323f204f5b9913a
soljatuulia <soljatuulia@gmail.com>
2023-05-25 14:05:12 +0300

    Add test for notifications

--- frontend/cypress/e2e/helsinki_bike_app.cy.js
+++ frontend/cypress/e2e/helsinki_bike_app.cy.js
@@ -16,53 +16,62 @@ describe('Helsinki Bike App', function() {
 		describe('sorts journeys', function() {
 			it('ascending by departure station name when clicking on appropriate column name', function() {
 				cy.get('#departureStation').click();
-				cy.contains('A.I. Virtasen aukio', { timeout: 10000 });
+				cy.contains('A.I. Virtasen aukio', { timeout: 18000 });
 			});
       
 			it('descending by departure station name when clicking twice on appropriate column name', function() {
 				cy.get('#departureStation').click().click();
-				cy.contains('Ympyrätalo', { timeout: 10000 });
+				cy.contains('Ympyrätalo', { timeout: 15000 });
 			});
 
 			it('ascending by return station name when clicking on appropriate column name', function() {
 				cy.get('#returnStation').click();
-				cy.contains('A.I. Virtasen aukio', { timeout: 10000 });
+				cy.contains('A.I. Virtasen aukio', { timeout: 18000 });
 			});
       
 			it('descending by return station name when clicking twice on appropriate column name', function() {
 				cy.get('#returnStation').click().click();
-				cy.contains('Ympyrätalo', { timeout: 10000 });
+				cy.contains('Ympyrätalo', { timeout: 15000 });
 			});
 
 			it('ascending by distance when clicking on appropriate column name', function() {
 				cy.get('#distance').click();
-				cy.contains('0.01', { timeout: 10000 });
+				cy.contains('0.01', { timeout: 18000 });
 			});
   
 			it('descending by distance when clicking twice on appropriate column name', function() {
 				cy.get('#distance').click().click();
-				cy.contains('3680.77', { timeout: 15000 });
+				cy.contains('3680.77', { timeout: 20000 });
 			});
   
 			it('ascending by duration when clicking on appropriate column name', function() {
 				cy.get('#duration').click();
-				cy.contains('0.23', { timeout: 10000 });
+				cy.contains('0.23', { timeout: 15000 });
 			});
       
 			it('descending by duration when clicking twice on appropriate column name', function() {
 				cy.get('#duration').click().click();
-				cy.contains('81495.48', { timeout: 17000 });
+				cy.contains('81495.48', { timeout: 18000 });
 			});
 		});
 
-		it('filters journeys by day and month', function() {
-			cy.get('#day-dropdown').click().get('#day').contains('1').click();
-			cy.get('#month-dropdown').click().get('#May').contains('May').click();
-			cy.get('#filter').click();
+		describe('filtering', function() {
+			beforeEach(function() {
+				cy.get('#day-dropdown').click().get('#day').contains('1').click();
+				cy.get('#month-dropdown').click().get('#May').contains('May').click();
+				cy.get('#filter').click();
+			});
 
-			cy.contains('Jätkäsaarenlaituri');
+			it('filters journeys by day and month', function() {
+				cy.contains('Jätkäsaarenlaituri', { timeout: 15000 });
+			});
+
+			it('resets filters when clicking reset button', function() {
+				cy.get('#reset').click();
+        
+				cy.contains('Select Day');
+			});      
 		});
-
 	});
 
 	describe('stations page', function() {
@@ -74,17 +83,22 @@ describe('Helsinki Bike App', function() {
 			cy.contains('Search for stations by typing the name or address below.');
 		});
 
-		it('search finds appropriate results', function() {
-			cy.get('#searchbar').type('ympyrätalo');
-			cy.contains('Porthaninrinne 1');
-		});
-
 		it('shows station details by clicking link', function() {
 			cy.get('#stationDetails').contains('station details').click();
-			cy.contains('Number of departures', { timeout: 10000 });
+			cy.contains('Number of departures', { timeout: 18000 });
+		});
+
+		describe('search', function() {
+			it('finds appropriate results', function() {
+				cy.get('#searchbar').type('ympyrätalo');
+				cy.contains('Porthaninrinne 1');
+			});
+      
+			it('shows user notification when no results are found', function() {
+				cy.get('#searchbar').type('pispala');
+				cy.contains('No stations found');
+			});
 		});
 	});
-
-
 
 });
