d6007c7d033ed764e2b76a45f5d62eb61423c476
Solja Virkkunen <soljatuulia@gmail.com>
2023-05-25 12:27:23 +0300

    Adjust timeout settings in Cypress tests

--- frontend/cypress/e2e/helsinki_bike_app.cy.js
+++ frontend/cypress/e2e/helsinki_bike_app.cy.js
@@ -14,44 +14,44 @@ describe('Helsinki Bike App', function() {
 		});
 
 		describe('sorts journeys', function() {
-      it('ascending by departure station name when clicking on appropriate column name', function() {
+			it('ascending by departure station name when clicking on appropriate column name', function() {
 				cy.get('#departureStation').click();
-				cy.contains('A.I. Virtasen aukio');
+				cy.contains('A.I. Virtasen aukio', { timeout: 10000 });
 			});
       
 			it('descending by departure station name when clicking twice on appropriate column name', function() {
 				cy.get('#departureStation').click().click();
-				cy.contains('Ympyrätalo');
+				cy.contains('Ympyrätalo', { timeout: 10000 });
 			});
 
-      it('ascending by return station name when clicking on appropriate column name', function() {
+			it('ascending by return station name when clicking on appropriate column name', function() {
 				cy.get('#returnStation').click();
-				cy.contains('A.I. Virtasen aukio');
+				cy.contains('A.I. Virtasen aukio', { timeout: 10000 });
 			});
       
 			it('descending by return station name when clicking twice on appropriate column name', function() {
 				cy.get('#returnStation').click().click();
-				cy.contains('Ympyrätalo');
+				cy.contains('Ympyrätalo', { timeout: 10000 });
 			});
 
 			it('ascending by distance when clicking on appropriate column name', function() {
 				cy.get('#distance').click();
-				cy.contains('0.01');
+				cy.contains('0.01', { timeout: 10000 });
 			});
   
 			it('descending by distance when clicking twice on appropriate column name', function() {
 				cy.get('#distance').click().click();
-				cy.contains('3680.77');
+				cy.contains('3680.77', { timeout: 15000 });
 			});
   
 			it('ascending by duration when clicking on appropriate column name', function() {
 				cy.get('#duration').click();
-				cy.contains('0.23');
+				cy.contains('0.23', { timeout: 10000 });
 			});
       
 			it('descending by duration when clicking twice on appropriate column name', function() {
 				cy.get('#duration').click().click();
-				cy.contains('68197.88');
+				cy.contains('81495.48', { timeout: 17000 });
 			});
 		});
 
@@ -81,7 +81,7 @@ describe('Helsinki Bike App', function() {
 
 		it('shows station details by clicking link', function() {
 			cy.get('#stationDetails').contains('station details').click();
-			cy.contains('Number of departures');
+			cy.contains('Number of departures', { timeout: 10000 });
 		});
 	});
 
